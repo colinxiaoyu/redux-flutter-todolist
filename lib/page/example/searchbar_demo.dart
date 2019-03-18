21dc718598a501a83bb675028f2bbfdb8a9baa1e
colin <516114821@qq.com>
2019-03-18 17:35:05 +0800

    完成 SearchBarDemo 页面

--- lib/page/example/searchbar_demo.dart
+++ lib/page/example/searchbar_demo.dart
@@ -1,17 +1,106 @@
 import 'package:flutter/material.dart';
 
-class SearchBarDemo extends StatelessWidget{
+const searchList = [
+  "ChengDu",
+  "ShangHai",
+  "BeiJing",
+  "TianJing",
+  "NanJing",
+  "ShenZheng"
+];
 
+const recentSuggest = ["suggest1", "suggest2"];
+
+class SearchBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('SearchBarDemo'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {showSearch(context: context, delegate: SearchBarDelegate());},
+            icon: Icon(Icons.search),
+          )
+        ],
       ),
       body: Center(
         child: Text('SearchBarDemo'),
       ),
     );
   }
+}
 
+class SearchBarDelegate extends SearchDelegate<String> {
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    assert(context != null);
+    final ThemeData theme = Theme.of(context);
+    assert(theme != null);
+    return theme.copyWith(
+      primaryColor: Colors.blue,
+      primaryIconTheme: theme.primaryIconTheme.copyWith(color: Colors.grey),
+      primaryColorBrightness: Brightness.light,
+      primaryTextTheme: theme.textTheme,
+    );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () => query = '',
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+          icon: AnimatedIcons.menu_arrow, progress: transitionAnimation),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100.0,
+        height: 100.0,
+        child: Card(
+          color: Colors.red,
+          child: Text(query),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentSuggest
+        : searchList.where((input) => input.startsWith(query)).toList();
+    return ListView.builder(
+      itemCount: suggestionList.length,
+        itemBuilder: (context,index)=>ListTile(
+          onTap: (){
+            query = suggestionList[index];
+            showResults(context);},
+          title: RichText(text: TextSpan(
+            text: suggestionList[index].substring(0,query.length),
+            style: TextStyle(
+              color: Colors.black,fontWeight: FontWeight.bold,
+            ),
+            children: [
+              TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color: Colors.grey)
+              )
+            ]
+          )),
+        ));
+  }
 }
