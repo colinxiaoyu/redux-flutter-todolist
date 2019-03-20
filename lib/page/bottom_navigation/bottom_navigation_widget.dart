2cb10a1743da56408be1f18d01f5db2b4b33aad2
colin <516114821@qq.com>
2019-03-20 16:32:42 +0800

    完成 BottomNavigation 页面

--- lib/page/bottom_navigation/bottom_navigation_widget.dart
+++ lib/page/bottom_navigation/bottom_navigation_widget.dart
@@ -1,19 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:redux_todolist/page/bottom_navigation/page_widget/airplay_screen.dart';
+import 'package:redux_todolist/page/bottom_navigation/page_widget/email_screen.dart';
+import 'package:redux_todolist/page/bottom_navigation/page_widget/home_screen.dart';
+import 'package:redux_todolist/page/bottom_navigation/page_widget/pages_screen.dart';
 
-class BottomNavigationWidget extends StatefulWidget{
+class BottomNavigationWidget extends StatefulWidget {
   @override
   _BottomNavigationWidgetState createState() {
     return _BottomNavigationWidgetState();
   }
-
 }
 
-class _BottomNavigationWidgetState extends State<BottomNavigationWidget>{
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+  final Color _bottomNavigationColor = Colors.blue;
+  int _currentIndex = 0;
+  List<Widget> list = List();
+
+  @override
+  void initState() {
+    list
+      ..add(HomeScreen())
+      ..add(EmailScreen())
+      ..add(PagesScreen())
+      ..add(AirPlayScreen());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('BottomNavigationWidget'),),
+      body: list[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _bottomNavigationColor,
+            ),
+            title: Text(
+              'Home',
+              style: TextStyle(color: _bottomNavigationColor),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.email,
+              color: _bottomNavigationColor,
+            ),
+            title: Text(
+              'Emai',
+              style: TextStyle(color: _bottomNavigationColor),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.pages,
+              color: _bottomNavigationColor,
+            ),
+            title: Text(
+              'PAGES',
+              style: TextStyle(color: _bottomNavigationColor),
+            )),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.airplay,
+              color: _bottomNavigationColor,
+            ),
+            title: Text(
+              'AIRPLAY',
+              style: TextStyle(color: _bottomNavigationColor),
+            )),
+      ],
+      currentIndex: _currentIndex,
+        onTap: (int index){
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.shifting,//BottomNavigationBarType.fixed
+
+      ),
+
+
     );
   }
-
 }
