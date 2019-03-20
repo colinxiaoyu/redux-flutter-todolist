655d423469d65067141b73d88cb46dbde29749c9
colin <516114821@qq.com>
2019-03-20 15:28:08 +0800

    完成 expansion

--- lib/page/example/expansion_demo/expansion_tile.dart
+++ lib/page/example/expansion_demo/expansion_tile.dart
@@ -16,24 +16,33 @@
 
 import 'package:flutter/material.dart';
 
+
+///**
+/// * ExpansionTile 内部使用了 ExpandIcon 带有旋转动画，可以仿制轮子
+///*/
 class ExpansionTileDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('expansion tile demo'),),
-      body: Center(
-        child: ExpansionTile(
-            title: Text('Expansion Tile'),
-            leading: Icon(Icons.ac_unit),
-            backgroundColor: Colors.white12,
-            children: <Widget>[
-              ListTile(
-                title: Text('list tile'),
-                subtitle: Text('subtitle'),
-              ),
-            ],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            ExpansionTile(
+              title: Text('Expansion Tile'),
+              leading: Icon(Icons.ac_unit),
+              backgroundColor: Colors.white12,
+              children: <Widget>[
+                ListTile(
+                  title: Text('list tile'),
+                  subtitle: Text('subtitle'),
+                ),
+              ],
 //          initiallyExpanded: true,
-        ),
+            ),
+
+          ],
+        )
       ),
     );
   }
